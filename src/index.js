137f1bfddda6557e13addaca82a7639ea205db7d
MisterAnthropy <tjcroley@outlook.com>
2020-12-19 14:26:12 -0800

    created js form for ingredients after submitting recipe form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const recipeForm = document.getElementById("recipe-form")
+const recipeInput = document.getElementById("recipe-input")
+const recipeList = document.getElementById("recipe-list")
 
 recipeForm.addEventListener("submit", submitRecipe)
 
 function submitRecipe(e) {
     e.preventDefault()
     console.log(e.target.children[0].value)
+    const li = document.createElement('li')
+    const p = document.createElement('p')
+    p.innerText = recipeInput.value
+    
+
+    const ingredientForm = document.createElement('form')
+    ingredientForm.innerHTML += `<input type="text"><input type="sumbit>`
+    
+    li.append(p, ingredientForm)
+
+    recipeList.appendChild(li)
 }
