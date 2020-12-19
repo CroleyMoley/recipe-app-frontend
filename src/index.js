a9528f0efa21a4d1b2f998b144e0b92bb7790756
MisterAnthropy <tjcroley@outlook.com>
2020-12-19 14:02:12 -0800

    created form to submit in html and js : '

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+const recipeForm = document.getElementById("recipe-form")
+
+recipeForm.addEventListener("submit", submitRecipe)
+
+function submitRecipe(e) {
+    e.preventDefault()
+    console.log(e.target.children[0].value)
+}
