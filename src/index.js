33c1d1b8fc617b0747f29821d0a598e7eb4f22f6
MisterAnthropy <tjcroley@outlook.com>
2020-12-20 16:18:43 -0800

    finished fetch request to post to rails server

--- src/index.js
+++ src/index.js
@@ -3,36 +3,43 @@ const recipeInput = document.getElementById("recipe-input")
 const recipeList = document.getElementById("recipe-list")
 const recipeUrl = `http://localhost:3000/recipes`
 
-recipeForm.addEventListener("submit", renderRecipe)
+recipeForm.addEventListener("submit", submitRecipe)
+//fetch request
+function submitRecipe() {
+    event.preventDefault()
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            title: recipeInput.value
+        })
+    }
+    fetch(recipeUrl, configObj)
+    .then(res => res.json())
+    .then(console.log)
+    renderRecipe()
+}
 //render recipe to the dom
-function renderRecipe(e) {
-    e.preventDefault()
-    console.log(e.target.children[0].value)
+function renderRecipe() {
+    event.preventDefault()
     const li = document.createElement('li')
     const p = document.createElement('p')
     p.innerText = recipeInput.value
-    console.log(e.target.children[0].value)
-
     const ingredientForm = document.createElement('form')
     ingredientForm.innerHTML += `<input type="text" id="ingredient-input"><input type="sumbit>`
-    
     ingredientForm.addEventListener("submit", submitIngredient)
     const ingredientList = document.createElement('ul')
     li.append(p, ingredientForm, ingredientList)
     recipeList.appendChild(li)
-
     recipeForm.reset()
 }
-//fetch request
-function submitRecipe() {
-
-}
-
 function submitIngredient(e) {
     e.preventDefault()
     const ingredientInput = e.target.children[0].value
     const ingredientList = e.target.nextElementSibling
-    
     const li = document.createElement('li')
     li.innerText = ingredientInput
     ingredientList.appendChild(li)
